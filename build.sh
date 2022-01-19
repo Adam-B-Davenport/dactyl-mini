052aa5ae0ba1e3d818c03cd457bc2f5db7988bdd
Adam Davenport <adam@galacticweapons.com>
2022-01-19 16:07:40 -0500

    Removing usb logging after settings reset

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cd /home/adam/dev/zmk/app
 
 west build -d build/$1 -b nice_nano_v2 -- -DSHIELD=storm_$1 -DZMK_CONFIG="$cfg"
 
-cp /home/adam/dev/zmk/app/build/$1/zephyr/zmk.uf2 /run/media/adam/NICENANO
-echo "copying /home/adam/dev/zmk/app/build/$1/zephyr/zmk.uf2 /run/media/adam/NICENANO"
+#cp /home/adam/dev/zmk/app/build/$1/zephyr/zmk.uf2 /run/media/adam/NICENANO
+#echo "copying /home/adam/dev/zmk/app/build/$1/zephyr/zmk.uf2 /run/media/adam/NICENANO"
+#echo "built from $cfg"
 
